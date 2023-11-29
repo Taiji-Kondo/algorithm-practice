3f213e20c8ce8a5f717831c56312bde1880b18ef
Taiji-Kondo <k.taiji1108@gmail.com>
2023-11-29 22:16:51 +0900

    bubble-sort

--- sort/bubble-sort/src/main.rs
+++ sort/bubble-sort/src/main.rs
@@ -1,5 +1,18 @@
 use rand::random;
 
 fn main() {
-    println!("Hello, world!");
+    let mut numbers: [u8; 10] = random();
+    println!("{:?}", bubble_sort(&mut numbers));
+}
+
+fn bubble_sort(numbers: &mut [u8]) -> &[u8] {
+    let numbers_len = numbers.len();
+    for i in 0..numbers_len {
+        for j in 0..numbers_len - i - 1  {
+            if numbers[j] > numbers[j + 1] {
+                (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+            }
+        }
+    }
+    return numbers;
 }
