ec566d05972a968ab47ef95a6434493c56f85c4a
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-03 18:00:20 +0900

    create selection sort

--- selection_sort/src/main.rs
+++ selection_sort/src/main.rs
@@ -1,3 +1,21 @@
+use rand::random;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers: [u8; 10] = random();
+    println!("{:?}", selection_sort(&mut numbers));
+}
+
+fn selection_sort(numbers: &mut [u8; 10]) -> &[u8; 10] {
+    let numbers_len: usize = numbers.len();
+
+    for i in 0..numbers_len {
+        let mut min_index: usize = i;
+        for j in i + 1..numbers_len {
+            if  numbers[min_index] > numbers[j] {
+                min_index = j;
+            }
+        }
+        (numbers[i], numbers[min_index]) = (numbers[min_index], numbers[i]);
+    }
+    numbers
 }
