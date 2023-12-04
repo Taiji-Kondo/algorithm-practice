5f18963a48f7ef589a5a0a5fd6f11963fb1546f0
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-05 08:54:55 +0900

    create insertion sort

--- insertion_sort/src/main.rs
+++ insertion_sort/src/main.rs
@@ -1,3 +1,19 @@
+use rand::random;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers: [u8; 10] = random();
+    println!("{:?}", insertion_sort(&mut numbers));
+}
+
+fn insertion_sort(numbers: &mut [u8]) -> &[u8] {
+    for i in 1..numbers.len() {
+        let temp: u8 = numbers[i];
+        let mut j: usize = i - 1;
+        while j >= 0 && numbers[j] > temp {
+            numbers[j + 1] = numbers[j];
+            j -= 1;
+        }
+        numbers[j + 1] = temp;
+    }
+    numbers
 }
